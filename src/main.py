fba1c5679be4fbd4654a3ea24a74aa8213647f1d
oskar <oskarxpd@gmail.com>
2022-07-08 23:02:36 +0200

    clean code

--- src/main.py
+++ src/main.py
@@ -3,9 +3,6 @@ from jbzd import Scraper
 
 scraper = Scraper()
 
-# with open('page.txt', 'r', encoding='utf-8') as f:
-#     page = f.read()
-
 page = scraper.getPage(1)
 
 articles = scraper.getArticles(page)
