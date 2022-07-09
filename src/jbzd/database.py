47a1cd048ae9a8a9ef798c4aad6204440e075141
oskar <oskarxpd@gmail.com>
2022-07-09 15:35:26 +0200

    create insertArticle method

--- src/jbzd/database.py
+++ src/jbzd/database.py
@@ -1,6 +1,18 @@
-import pymongo
+from pymongo import MongoClient
+from .article import Article
 
 
 class Database:
-    def __init__(self, name='jbzd') -> None:
-        pass
+    def __init__(self, uri, name='jbzd') -> None:
+        self.client = MongoClient(uri)
+        self.db = self.client[name]
+
+    def insertArticle(self, article: Article):
+        articles = self.db['articles']
+        doc = {
+            '_id': article._id,
+            'title': article.title,
+            'src': article.src,
+            'type': article.type
+        }
+        articles.insert_one(doc)
